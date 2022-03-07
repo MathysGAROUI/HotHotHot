2a4acf243daf1436223ad7d36a1dbdb71d8b9d7b
Mathys GAROUI <mathys.garoui@etu.univ-amu.fr>
2022-03-07 12:14:56 +0100

    Graph slice

--- controllers/graphics.js
+++ controllers/graphics.js
@@ -1,15 +1,20 @@
 function refreshGraphics(previousLabel){
+
+    var dates = JSON.parse(localStorage.getItem('capteurs')).dates;
+    var cpt1 = JSON.parse(localStorage.getItem('capteurs')).cpt1;
+    var cpt2 = JSON.parse(localStorage.getItem('capteurs')).cpt2;
+
     new Chart(document.getElementById("line-chart"), {
         type: 'line',
         data: {
-          labels: JSON.parse(localStorage.getItem('capteurs')).dates,
+          labels: dates.slice(-20),
           datasets: [{ 
-              data: JSON.parse(localStorage.getItem('capteurs')).cpt1,
+              data: cpt1.slice(-20),
               label: "Capteur interieur",
               borderColor: "#3e95cd",
               fill: false
             }, { 
-              data: JSON.parse(localStorage.getItem('capteurs')).cpt2,
+              data: cpt2.slice(-20),
               label: "Capteur exterieur",
               borderColor: "#8e5ea2",
               fill: false
@@ -24,5 +29,7 @@ function refreshGraphics(previousLabel){
         }
       });
 }
+
+
 
 HotSDK.refreshGraphics = refreshGraphics;
