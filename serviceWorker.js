5ae09c6ac6c057fe55fa22500b78060c20ff503b
Mathys GAROUI <mathys.garoui@etu.univ-amu.fr>
2022-03-31 00:21:00 +0200

    debug

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,20 @@
 var STATIC_CACHE_CONTAINER = "static_v1"
 var STATIC_FILES = [
-    "/"
+    "/",
+    "/controllers/",
+    "/controllers/connection.js",
+    "/controllers/global.js",
+    "/controllers/graphics.js",
+    "/controllers/layout.js",
+    "/controllers/notification.js",
+    "/controllers/parsing.js",
+    "/controllers/pwa.js",
+    "/controllers/refresher.js",
+    "/controllers/storage.js",
+    "/models/",
+    "/models/Sensor.js",
+    "/style/",
+    "/style/layout.css"
 ]
 
 self.addEventListener('install', function(event){
