f11bc7a5feb0a388f4d8e05b5f2eb0516c48dff2
Mathys GAROUI <mathys.garoui@etu.univ-amu.fr>
2022-03-30 23:47:27 +0200

    Offline PWA test

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,16 @@
+var STATIC_CACHE_CONTAINER = "static_v1"
+var STATIC_FILES = [
+    "/",
+    "/index.html"
+]
+
 self.addEventListener('install', function(event){
-    console.log("service worker installed", event)
+    event.waitUntil(
+        caches.open(STATIC_CACHE_CONTAINER)
+            .then(function(cache){
+                cache.addAll(STATIC_FILES)
+            })
+    )
 })
 
 self.addEventListener('activate', function(event){
