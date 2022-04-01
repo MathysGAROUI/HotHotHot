05636a42772d56896a8894489ff571537433425c
Mathys GAROUI <mathys.garoui@etu.univ-amu.fr>
2022-04-02 00:04:18 +0100

    Test

--- controllers/router.js
+++ controllers/router.js
@@ -23,7 +23,7 @@ function disableAllPages(){
 
 document.getElementById("historyPageButton").onclick = function (){
     switchPage('history');
-    HotSDK.sendNotif();
+    HotSDK.sendNotif('hihihiha');
 }
 
 document.getElementById("mainPageButton").onclick = function (){
